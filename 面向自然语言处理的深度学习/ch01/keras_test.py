cfdd48aad9809bf1b08d4ef491d8dd4e4dfb42f9
peter <peter19881004@126.com>
2021-05-14 19:06:32 +0800

    update

---
+++ 面向自然语言处理的深度学习/ch01/keras_test.py
@@ -0,0 +1,77 @@
+"""
+Keras 的示例代码
+"""
+
+# 从keras导入所需的库，层和模型
+import keras
+from keras.layers import Dense
+# Keras Sequential 顺序模型
+# 顺序模型是多个网络层的线性堆叠
+from keras.models import Sequential
+import numpy as np
+
+# Dataset link: # http://archive.ics.uci.edu/ml/datasets/Blood+Transfusion+Service+Centers
+# Save the dataset as a.cssv file
+train_ = np.genfromtxt('transfusion.csv', delimiter=',')
+print(train_.shape)
+X = train_[:, 0:4]
+Y = train_[:, 4]
+print(X)
+
+# 使用Keras创建我们的第一个MLP模型
+# mlp_keras = Sequential()
+# mlp_keras.add(Dense(8,
+#                     input_dim=4,
+#                     init='uniform',
+#                     activation='relu'
+#                     ))
+# mlp_keras.add(Dense(6,
+#                     init="uniform",
+#                     activation='relu'
+#                     ))
+# mlp_keras.add(Dense(1,
+#                     init="uniform",
+#                     activation='sigmoid'
+#                     ))
+#
+# mlp_keras.compile(loss="binary_crossentropy",
+#                   optimizer='adam',
+#                   metrics=['accuracy'])
+#
+# mlp_keras.fit(X, Y, nb_epoch=200, batch_size=8, verbose=0)
+# accurary = mlp_keras.evaluate(X, Y)
+# print("Accurary: %.2f%%" % (accurary[1] * 100))
+
+# 使用Keras创建我们的第一个MLP模型
+from keras.optimizers import SGD
+opt = SGD(lr=0.01)
+
+mlp_keras = Sequential()
+mlp_keras.add(Dense(8,
+                    input_dim=4,
+                    init='uniform',
+                    activation='relu'
+                    ))
+mlp_keras.add(Dense(6,
+                    init="uniform",
+                    activation='relu'
+                    ))
+mlp_keras.add(Dense(1,
+                    init="uniform",
+                    activation='sigmoid'
+                    ))
+
+mlp_keras.compile(loss="binary_crossentropy",
+                  optimizer=opt,
+                  metrics=['accuracy'])
+
+mlp_keras.fit(X, Y,
+              validation_split=0.3,
+              epochs=150,
+              batch_size=10,
+              verbose=0)
+results_optim = mlp_keras.evaluate(X, Y)
+print("Accurary: %.2f%%" % (results_optim[1] * 100))
+
+
+
---
+++ 面向自然语言处理的深度学习/ch01/tf_test.py
@@ -0,0 +1,39 @@
+"""
+Tf的示例代码
+"""
+import tensorflow as tf
+hello = tf.constant("Hello,Tensors!")
+sess = tf.compat.v1.Session()
+print(sess.run(hello))
+
+# Mathematical computation
+# 数学运算
+a = tf.constant(10)
+b = tf.constant(32)
+print(sess.run(a + b))
+
+import numpy as np
+# 创建numpy数组
+mat_1 = 10 * np.random.random_sample((3, 4))
+mat_2 = 10 * np.random.random_sample((4, 6))
+
+# 创建一对常量运算，并包括上述制成的矩阵
+tf_mat_1 = tf.constant(mat_1)
+tf_mat_2 = tf.constant(mat_2)
+
+# Multiplying Tensorflow matrices with matrix multiplication
+# 将Tensorflow矩阵与矩阵相乘
+tf_mat_prod = tf.matmul(tf_mat_1, tf_mat_2)
+sess = tf.Session()
+
+# run（）执行必需的操作，并执行将输出存储在'mult_matrix'变量中的请求
+mult_matrix = sess.run(tf_mat_prod)
+print(mult_matrix)
+
+# Performing constant operations with the addition and subtraction of two constant
+# 用两个常数的加法和减法执行常数运算
+a = tf.constant(10)
+b = tf.constant(20)
+print("Addition of constant 10 and 20 is %i" % sess.run(a + b))
+print("Subtration of constant 10 and 20 is %i" % sess.run(a - b))
+sess.close()
