c84ba0e502d706c766f8a3147b685d6c412f252d
peter19881004@126.com <lszrl20141007>
2021-06-22 21:40:11 +0800

    change

---
+++ 面向自然语言处理的深度学习/ch04/model.py
@@ -0,0 +1,273 @@
+"""
+https://tangshusen.me/2019/03/09/tf-attention/
+"""
+import tensorflow as tf
+
+# import tensorflow_addons as tfa
+import keras
+
+class SeqModel():
+    """
+    定义seq2seq模型，训练机器人
+    """
+    def __init__(self):
+        pass
+
+    def model_inputs(self):
+        """
+        模型输入定义
+        :return:
+
+        """
+        tf.compat.v1.disable_eager_execution()
+        input_data = tf.compat.v1.placeholder(tf.int32, [None, None], name='input')
+        targets = tf.compat.v1.placeholder(tf.int32, [None, None], name='targets')
+        # 学习率
+        lr = tf.compat.v1.placeholder(tf.float32, name='learning_rate')
+        keep_prob = tf.compat.v1.placeholder(tf.float32, name='keep_prob')
+        return input_data, targets, lr, keep_prob
+
+
+    def process_encoding_input(self, target_data, vocab_to_int, batch_size):
+        """
+        删除每个批次中最后一个单词ID，并在每个批次开头追加<GO>标记
+        :param target_data:
+        :param batch_size:
+        :param vocab_to_int:
+        :return:
+        """
+
+        # 对张量进行切片操作
+        ending = tf.strided_slice(target_data, [0, 0], [batch_size, -1], [1, 1])
+        dec_input = tf.concat([tf.fill([batch_size, 1], vocab_to_int['<GO>']), ending], 1)
+
+        return dec_input
+
+    def encodig_layer(self, rnn_inputs, rnn_size, num_layers, keep_prod, sequence_length):
+        """
+        定义模型，使用LSTM神经元和双向编码器来定义Seq2Seq模型的编码层
+        :return:
+        """
+        lstm = tf.contrib.rnn.BasicLSTMCell(rnn_size)
+        drop = tf.contrib.rnn.DropoutWrapper(lstm, input_keep_prob=keep_prod)
+
+        # 多层循环神经网络编码器
+        enc_cell = tf.contrib.rnn.MultiRNNCell([drop] * num_layers)
+        # 双向RNN
+        enc_output, enc_state = tf.nn.bidirectional_dynamic_rnn(
+            cell_fw=enc_cell,
+            cell_bw=enc_cell,
+            sequence_length=sequence_length,
+            inputs=rnn_inputs,
+            dtype=tf.float32
+        )
+
+        return enc_output, enc_state
+
+    def decoding_layer_train(self, encoder_state, dec_cell, dec_embed_input,
+                             sequence_length, decoding_scope,
+                             output_fn, keep_prob, batch_size):
+        """
+        解码器层 --- 加入attention机制
+        :param encoder_state:
+        :param dec_cell:
+        :param dec_embed_input:
+        :param sequence_length:
+        :param decoding_scope:
+        :param output_fn:
+        :param keep_prob:
+        :param batch_size:
+        :return:
+        """
+        # attention 矩阵 U、V、K
+        attention_states = tf.zeros([batch_size, 1, dec_cell.output_size])
+        att_keys, att_vals, att_score_fn, att_construct_fn = tf.contrib.seq2seq.prepare_attention(
+            attention_states,
+            attention_option="bahdanau",
+            num_units=dec_cell.output_size
+        )
+
+        train_decoder_fn = tf.contrib.seq2seq.attention_decoder_fn_train(encoder_state[0],
+                                                                         att_keys,
+                                                                         att_vals,
+                                                                         att_score_fn,
+                                                                         att_construct_fn,
+                                                                         name='attn_dec_train')
+
+        train_pred, _, _ = tf.contrib.seq2seq.dynamic_rnn_decoder(dec_cell,
+                                                                  train_decoder_fn,
+                                                                  dec_embed_input,
+                                                                  sequence_length,
+                                                                  scope=decoding_scope)
+        train_pred_drop = tf.nn.dropout(train_pred, keep_prob)
+
+        return output_fn(train_pred_drop)
+
+    def decoding_layer_infer(self, encoder_state, dec_cell,
+                             dec_embeddings, start_of_seqence_id,
+                             end_of_sequence_id, maximum_length,
+                             vocab_size, decoding_scope,
+                             output_fn, keep_prob, batch_size):
+        """
+        为所查询对问题创建合适对回复，
+        :param encoder_state:
+        :param dec_cell:
+        :param dec_embeddings:
+        :param start_of_seqence_id:
+        :param end_of_sequence_id:
+        :param maximum_length:
+        :param vocab_size:
+        :param decoding_scope:
+        :param output_fn:
+        :param keep_prod:
+        :param batch_size:
+        :return:
+        """
+        attention_states = tf.zeros([batch_size, 1, dec_cell.output_size])
+        att_keys, att_vals, att_score_fn, att_construct_fn = tf.contrib.seq2seq.prepare_attention(
+            attention_states,
+            attention_option="bahdanau",
+            num_units=dec_cell.output_size
+        )
+
+        infer_decoder_fn = tf.contrib.seq2seq.attention_decoder_fn_inference(
+            output_fn,
+            encoder_state[0],
+            att_keys,
+            att_vals,
+            att_score_fn,
+            att_construct_fn,
+            dec_embeddings,
+            start_of_seqence_id,
+            end_of_sequence_id,
+            maximum_length,
+            vocab_size,
+            name='attn_dec_inf'
+        )
+
+        infer_logits, _, _ = tf.contrib.seq2seq.dynamic_rnn_decoder(
+            dec_cell, infer_decoder_fn, scope=decoding_scope
+        )
+        return infer_logits
+
+    def decoding_layer(self, dec_embed_input, dec_embedding, encoder_state, vocab_sise,
+                       sequence_length, rnn_size, num_layers, vocab_to_int, keep_prob, batch_size):
+        """
+        创建推理和训练分对数, 并使用给定对标准差通过截断的正态分布来初始化权重和偏差
+        :return:
+        """
+        with tf.variable_scope("decoding") as decoding_scope:
+            lstm = tf.contrib.rnn.BasicLSTMCell(rnn_size)
+            drop = tf.contrib.rnn.DropoutWrapper(lstm, input_keep_prob=keep_prob)
+            dec_cell = tf.contrib.rnn.MultiRNNCell([drop] * num_layers)
+
+            weights = tf.truncated_normal_initializer(stddev=0.1)
+            biases = tf.zeros_initializer()
+
+            output_fn = lambda x: tf.contrib.layers.fully_connected(
+                x,
+                vocab_sise,
+                None,
+                scope=decoding_scope,
+                weights_initializer=weights,
+                biases_initializer=biases
+            )
+        # 训练Decoder
+        train_logits = self.decoding_layer_train(encoder_state,
+                                                 dec_cell,
+                                                 dec_embed_input,
+                                                 sequence_length,
+                                                 decoding_scope,
+                                                 output_fn,
+                                                 keep_prob,
+                                                 batch_size)
+        decoding_scope.reuse_varibles()
+        infer_logits = self.decoding_layer_infer(encoder_state,
+                                                 dec_cell,
+                                                 dec_embedding,
+                                                 vocab_to_int['<GO>'],
+                                                 vocab_to_int['<EOS>'],
+                                                 sequence_length - 1,
+                                                 vocab_sise,
+                                                 decoding_scope,
+                                                 output_fn,
+                                                 keep_prob,
+                                                 batch_size)
+
+        return train_logits, infer_logits
+
+    def seq2seq_model(self, input_data, target_data,
+                       keep_prob, batch_size,
+                       sequence_length,
+                       answers_vocab_size,
+                       questions_vocab_size,
+                       enc_embedding_size,
+                       dec_embedding_size,
+                       rnn_size,
+                       num_layers,
+                       questions_vocab_to_int):
+        """
+        用户将编码器等串联起来，使用随机均匀分布来初始化词嵌入过程。
+        :param input_data:
+        :param target_data:
+        :param keep_prob:
+        :param batch_size:
+        :param sequence_length:
+        :param answers_vocab_size:
+        :param questions_vocab_size:
+        :param enc_embedding_size:
+        :param dec_embedding_size:
+        :param rnn_size:
+        :param num_layers:
+        :param questions_vocab_to_int:
+        :return:
+        """
+        # tf.contrib.layers.embed_sequence 为句子产生词嵌入
+        enc_embed_input = tf.contrib.layers.embed_sequence(
+            input_data,
+            vocab_size=answers_vocab_size + 1,
+            embed_dim=enc_embedding_size,
+            initializer=tf.random_normal_initializer(0, 1)
+        )
+        # with tf.variable_scope(
+        #         None, 'EmbedSequence', [input_data], reuse=None):
+        #     shape = [answers_vocab_size + 1, enc_embedding_size]
+        #
+        #     embeddings = tf.model_variables(
+        #         'embeddings',
+        #         shape=shape,
+        #         initializer=tf.random_normal_initializer(0, 1),
+        #         regularizer=None,
+        #         trainable=True)
+        #
+        #     enc_embed_input = tf.embedding_lookup(embeddings, input_data)
+
+        enc_output, enc_state = self.encodig_layer(enc_embed_input,
+                                       rnn_size,
+                                       num_layers,
+                                       keep_prob,
+                                       sequence_length)
+
+        dec_input = self.process_encoding_input(target_data,
+                                                questions_vocab_to_int,
+                                                batch_size)
+
+        dec_embeddings = tf.Variable(
+            tf.random_uniform([questions_vocab_size + 1, dec_embedding_size], 0, 1)
+        )
+        dec_embed_input = tf.nn.embedding_lookup(
+            dec_embeddings,
+            dec_input
+        )
+
+        train_logits, infer_logits = self.decoding_layer(dec_embed_input,
+                                                         dec_embeddings,
+                                                         enc_state,
+                                                         questions_vocab_size,
+                                                         sequence_length,
+                                                         rnn_size,
+                                                         num_layers,
+                                                         questions_vocab_to_int,
+                                                         keep_prob,
+                                                         batch_size)
+        return train_logits, infer_logits
---
+++ 面向自然语言处理的深度学习/ch05/sentiment_test.py
@@ -0,0 +1,291 @@
+from keras.datasets import imdb
+
+# load_data函数使用8个参数来自定义评论数据集
+(X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=1000, index_from=3)
+
+vocab_to_int = imdb.get_word_index()
+vocab_to_int = {k: (v + 3) for k, v in vocab_to_int.items()}
+
+vocab_to_int['<PAD>'] = 0
+vocab_to_int['<GO>'] = 1
+vocab_to_int['<UNK>'] = 2
+int_to_vocab = {value: key for key, value in vocab_to_int.items()}
+print(' '.join(int_to_vocab[id] for id in X_train[0]))
+
+
+import tensorflow as tf
+print(tf.__version__)
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+tf.compat.v1.disable_eager_execution()
+
+(X_train, y_train), (X_test, y_test) = imdb.load_data(path="imdb_full.pkl", num_words=None, skip_top=0,
+                                                      maxlen=None, seed=113, start_char=1, oov_char=2, index_from=3)
+
+print(X_train[:2])
+
+
+t = [item for sublist in X_train for item in sublist]
+vocabulary = len(set(t)) + 1
+# 数量分布
+a = [len(x) for x in X_train]
+plt.plot(a)
+# plt.show()
+
+max_length = 200
+
+x_filter = []
+y_filter = []
+# 不过max_length长度补0，超过则截断
+for i in range(len(X_train)):
+    if len(X_train[i]) < max_length:
+        a = len(X_train[i])
+        X_train[i] = X_train[i] + [0] * (max_length - a)
+    elif len(X_train[i]) >= max_length:
+        X_train[i] = X_train[i][0: max_length]
+    else:
+        continue
+    x_filter.append(X_train[i])
+    y_filter.append(y_train[i])
+
+# 超参数定义
+embedding_size = 100
+n_hidden = 254
+learning_rate = 0.06
+training_iters = 100000
+batch_size = 32
+beta = 0.0001
+# 定义模型体系结构和数据集相关的其他参数
+# 时间步长参数
+n_steps = max_length
+# 分类的类数
+n_classes = 2
+# 自注意力MLP的隐藏层中的单元数
+da = 30
+# 嵌入矩阵中的行数
+r = 30
+display_step = 10
+hidden_units = 3000
+
+# 将训练数据集和标签转换为阵列后变换及编码的所需格式
+y_train = np.asarray(pd.get_dummies(y_filter))
+X_train = np.asarray([np.asarray(g) for g in x_filter])
+
+logs_path = 'recent_logs'
+
+
+class DataIterator:
+    """
+    产生batch数据
+    """
+    def __init__(self, data1, data2, batch_size):
+        """
+         Takes data sources and batch_size as arguments
+        :param data1:
+        :param data2:
+        :param batch_size:
+        """
+        self.data1 = data1
+        self.data2 = data2
+        self.batch_size = batch_size
+        self.iter = self.make_random_iter()
+
+    def next_batch(self):
+        try:
+            idxs = next(self.iter)
+        except StopIteration:
+            self.iter = self.make_random_iter()
+            idxs = next(self.iter)
+
+        X = [self.data1[i] for i in idxs]
+        Y = [self.data2[i] for i in idxs]
+
+        X = np.array(X)
+        Y = np.array(Y)
+
+        return X, Y
+
+    def make_random_iter(self):
+        print(self.data1)
+        splits = np.arange(self.batch_size, len(self.data1), self.batch_size)
+        it = np.split(np.random.permutation(range(len(self.data1))), splits)[:-1]
+        return iter(it)
+
+# 初始化权重和偏差
+with tf.name_scope("weights"):
+    Win = tf.Variable(
+        tf.random.uniform([n_hidden * r, hidden_units], -1/np.sqrt(n_hidden), 1/np.sqrt(n_hidden)),
+        name="W-input"
+    )
+
+    Wout = tf.Variable(
+        # tf随机函数
+        tf.random.uniform([hidden_units, n_classes], -1/np.sqrt(n_hidden), 1/np.sqrt(n_hidden)),
+        name="W-out"
+    )
+
+    Ws1 = tf.Variable(
+        tf.random.uniform([da, n_hidden], -1/np.sqrt(da), 1/np.sqrt(da)),
+        name="Ws1"
+    )
+
+    Ws2 = tf.Variable(
+        tf.random.uniform([r, da], -1/np.sqrt(r), 1/np.sqrt(r)),
+        name="Ws2"
+    )
+
+
+with tf.name_scope("biases"):
+    biasesout = tf.Variable(tf.random.uniform([n_classes]), name='biases-out')
+    biasesin = tf.Variable(tf.random.uniform([hidden_units]), name='biases-in')
+
+with tf.name_scope("input"):
+    x = tf.compat.v1.placeholder("int32", [32, max_length], name='x-input')
+    y = tf.compat.v1.placeholder("int32", [32, 2], name="y-input")
+
+with tf.name_scope("embedding"):
+    embedings = tf.Variable(
+        tf.random.uniform([vocabulary, embedding_size], -1, 1),
+        name='embeddings'
+    )
+    embed = tf.nn.embedding_lookup(embedings, x)
+
+
+def length(sequence):
+    """
+
+    :param sequence: 句子
+    :return:
+    """
+    # Computing maximum of elements across dimensions of a tensor
+    used = tf.sign(
+        tf.compat.v1.reduce_max(tf.abs(sequence), reduction_indices=2)
+    )
+    length = tf.compat.v1.reduce_sum(used, reduction_indices=1)
+    length = tf.cast(length, tf.int32)
+
+    return length
+
+
+# 下面的代码重算权重和偏差
+with tf.compat.v1.variable_scope("forward", reuse=True):
+
+    lstm_fw_cell = tf.compat.v1.nn.rnn_cell.LSTMCell(n_hidden)
+    # lstm_fw_cell = tf.keras.layers.LSTMCell(n_hidden)
+
+with tf.name_scope("model"):
+    # outputs 维度：32 * 200* 200 batch_size * n_steps（句子长度） * 隐藏层大小
+    outputs, states = tf.compat.v1.nn.dynamic_rnn(lstm_fw_cell,
+                                      embed,
+                                      sequence_length=length(embed),
+                                      dtype=tf.float32,
+                                      time_major=False)
+    print("输出维度")
+    print(outputs.shape)
+    # 下一步我们将隐藏向量与Ws1 相乘，重塑Ws1
+    h = tf.compat.v1.nn.tanh(
+        # 对矩阵转置
+        tf.compat.v1.transpose(
+            tf.compat.v1.reshape(
+                # tf.matmul 矩阵相乘
+                tf.compat.v1.matmul(Ws1, tf.compat.v1.reshape(outputs, [n_hidden, batch_size * n_steps])),
+                [da, batch_size, n_steps]
+            ), [1, 0, 2]
+        )
+    )
+
+    a = tf.reshape(
+        tf.matmul(Ws2, tf.reshape(h, [da, batch_size * n_steps])),
+        [batch_size, r, n_steps]
+    )
+
+    def fn3(a, x):
+        """
+
+        :param a:
+        :param x:
+        :return:
+        """
+        print(a)
+        print(x)
+
+        return tf.nn.softmax(x)
+
+    # a = softmax(Ws2 * tanh(Ws1 Ht)) Ht代表H的转置 H= （h1,h2,h3)
+    h3 = tf.scan(fn3, a)
+
+# 定义嵌入矩阵
+with tf.name_scope("flattening"):
+    h4 = tf.matmul(h3, outputs)
+    last = tf.reshape(h4, [-1, r * n_hidden])
+
+# 定义注意力机制
+with tf.name_scope("MLP"):
+    tf.nn.dropout(last, rate=1 - 0.5, noise_shape=None, seed=None, name=None)
+    pred1 = tf.nn.sigmoid(tf.matmul(last, Win) + biasesin)
+    pred = tf.matmul(pred1, Wout) + biasesout
+
+# 定义损失函数及优化器
+with tf.name_scope("cross"):
+    cost = tf.reduce_mean(
+        tf.compat.v1.nn.softmax_cross_entropy_with_logits_v2(logits=pred, labels=y) + beta * tf.nn.l2_loss(Ws2)
+    )
+
+with tf.name_scope("Train"):
+    optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=learning_rate)
+    # 计算梯度
+    gvs = optimizer.compute_gradients(cost)
+    # clip_by_norm 对梯度进行裁剪
+    capped_gvs = [(tf.clip_by_norm(grad, 0.5), var) for grad, var in gvs]
+    optimizer.apply_gradients(capped_gvs)
+    optimized = optimizer.minimize(cost)
+
+with tf.name_scope("Accuracy"):
+    correct_pred = tf.equal(
+        tf.argmax(pred, 1),
+        tf.argmax(y, 1)
+    )
+
+    accuracy = tf.reduce_mean(
+        tf.cast(correct_pred, tf.float32)
+    )
+
+tf.compat.v1.summary.scalar("cost", cost)
+tf.compat.v1.summary.scalar("accuracy", accuracy)
+
+# merge all summaries into a single "summary operation" which we can execute in a session
+summary_op = tf.compat.v1.summary.merge_all()
+training_iter = DataIterator(X_train, y_train, batch_size)
+init = tf.compat.v1.global_variables_initializer()
+
+# This could give warning if in case the required port is being used already
+# Running the command again or releasing the port before the subsequent run should solve the purpose
+with tf.compat.v1.Session() as sess:
+    sess.run(init)
+    writer = tf.compat.v1.summary.FileWriter(logs_path, graph=tf.compat.v1.get_default_graph())
+
+    step = 1
+    # Keep training until reach max iterations
+    while step * batch_size < training_iters:
+        batch_x, batch_y = training_iter.next_batch()
+        # 计算最小梯度
+        sess.run(optimized,
+                 feed_dict={x: batch_x, y: batch_y})
+        # Executing the summary operation in the session
+        summary = sess.run(
+            summary_op, feed_dict={x: batch_x, y: batch_y}
+        )
+
+        writer.add_summary(summary, step * batch_size)
+        if step % display_step == 2:
+            # Calculate batch accuracy
+            acc = sess.run(accuracy, feed_dict={x: batch_x, y: batch_y})
+            # Calculate batch loss
+            loss = sess.run(cost, feed_dict={x: batch_x, y: batch_y})
+            print("Iter " + str(step * batch_size) + \
+                  ", Minibatch Loss= " + "{:.6f}".format(loss) + \
+                  ", Training Accuracy= " + "{:.2f}".format(acc * 100) + "%")
+        step += 1
+    print("Optimization Finished!")
